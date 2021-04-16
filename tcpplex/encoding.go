5ebeb1018aabd43163ce8a7f9dd46615109025c0
Tuan Vuong <vanhtuan0409@gmail.com>
2021-04-16 13:28:10 +0700

    add

---
+++ tcpplex/encoding.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"io"
+)
+
+func encode(w io.Writer, p packet) error {
+	return nil
+}
+
+func decode(r io.Reader, p interface{}) error {
+	return nil
+}
--- tcpplex/plex.go
+++ tcpplex/plex.go
@@ -9,6 +9,10 @@ import (
 var (
 	ErrHandleClosed = errors.New("handle closed")
 	ErrServerClosed = errors.New("server closed")
+
+	FSYNC byte = 1
+	FACK  byte = 10
+	FFIN  byte = 100
 )
 
 type packet struct {
@@ -77,52 +81,108 @@ func (h *Handle) Close() error {
 
 func (h *Handle) send(p packet) error {
 	// can implement retry and SYN-ACK here
-	return nil
+	select {
+	case h.out <- p:
+		return nil
+	default:
+		return ErrHandleClosed
+	}
 }
 
 func (h *Handle) receive() (packet, error) {
-	// can implement retry and SYN-ACK here
-	return packet{}, nil
+	select {
+	case p := <-h.in:
+		return p, nil
+	default:
+		return packet{}, ErrHandleClosed
+	}
 }
 
 type MultiPlexClient struct {
-	conn  net.Conn
-	maxId int
+	conn   net.Conn
+	maxId  int
+	bufOut chan packet
 
 	hs map[int]*Handle
 }
 
 func NewMultiplexClient(conn net.Conn) *MultiPlexClient {
-	return &MultiPlexClient{
-		conn: conn,
-		hs:   make(map[int]*Handle),
+	c := &MultiPlexClient{
+		conn:   conn,
+		hs:     make(map[int]*Handle),
+		bufOut: make(chan packet, 100),
 	}
+	go c.loopRead()
+	go c.loopWrite()
+	return c
 }
 
 // NewHandle equal to Dial
 func (c *MultiPlexClient) NewHandle() (*Handle, error) {
+	hOut := make(chan packet, 100)
 	h := &Handle{
 		id:  c.maxId,
 		in:  make(chan packet, 100),
-		out: make(chan packet, 100),
+		out: hOut,
 	}
 	c.maxId += 1
 	if err := h.Dial(); err != nil {
 		return nil, err
 	}
+
+	// send to main loopWrite
+	go func() {
+		for p := range hOut {
+			c.bufOut <- p
+		}
+	}()
+
 	return h, nil
 }
 
-type MultiPlexServer struct {
-	conn net.Conn
+func (c *MultiPlexClient) loopRead() {
+	for {
+		p := &packet{}
+		if err := decode(c.conn, p); err != nil {
+			break
+		}
 
+		h, ok := c.hs[p.handleId]
+		if !ok {
+			// just ignore invalid packet
+			continue
+		}
+
+		// handle multiple flag here
+
+		h.in <- *p
+	}
+}
+
+func (c *MultiPlexClient) loopWrite() {
+	for p := range c.bufOut {
+		// handle multiple packet flag here
+		encode(c.conn, p)
+	}
+}
+
+type MultiPlexServer struct {
+	conn   net.Conn
+	bufOut chan packet
+
+	hs      map[int]*Handle
 	backlog chan *Handle
 }
 
 func NewMultiplexServer(conn net.Conn) *MultiPlexServer {
-	return &MultiPlexServer{
-		conn: conn,
+	s := &MultiPlexServer{
+		conn:   conn,
+		hs:     make(map[int]*Handle),
+		bufOut: make(chan packet, 100),
 	}
+	go s.loopRead()
+	go s.loopWrite()
+	return s
 }
 
 func (s *MultiPlexServer) Accept() (*Handle, error) {
@@ -131,5 +191,31 @@ func (s *MultiPlexServer) Accept() (*Handle, error) {
 		return h, nil
 	default:
 		return nil, ErrServerClosed
+	}
+}
+
+func (s *MultiPlexServer) loopRead() {
+	for {
+		p := &packet{}
+		if err := decode(s.conn, p); err != nil {
+			break
+		}
+
+		h, ok := s.hs[p.handleId]
+		if !ok {
+			// just ignore invalid packet
+			continue
+		}
+
+		// handle multiple flag here
+
+		h.in <- *p
+	}
+}
+
+func (s *MultiPlexServer) loopWrite() {
+	for p := range s.bufOut {
+		// handle multiple packet flag here
+		encode(s.conn, p)
 	}
 }
